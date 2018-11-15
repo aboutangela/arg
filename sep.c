c4b92209fbb464ba3735680104a9bc765989df80
Angela Li <32581248+aboutangela@users.noreply.github.com>
2018-11-14 19:23:13 -0500

    fixed indentation

--- sep.c
+++ sep.c
@@ -6,7 +6,7 @@
 char ** parse_args( char * line ){
 	char ** ar = malloc(5 * sizeof(char *));
 	int count = 0;
-  while(line){
+  	while(line){
 		ar[count] = strsep(&line, " ");
 		count++;
 	}
@@ -15,7 +15,7 @@ char ** parse_args( char * line ){
 
 int main(){
 	char l[100] = "ls -l -a";
-  char ** args = parse_args(l);
-  execvp(args[0], args);
+  	char ** args = parse_args(l);
+  	execvp(args[0], args);
 	return 0;
 }
